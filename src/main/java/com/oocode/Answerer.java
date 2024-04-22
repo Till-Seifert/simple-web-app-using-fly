e34ca030f5950cdfeb70a0795bdb047be04308a7
Till <Till.Seifert@d-fine.de>
2024-04-22 16:55:30 +0100

    Extending possible questions.

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -2,6 +2,12 @@ package com.oocode;
 
 public class Answerer {
     public String answerFor(String question) {
-        return "Till S";
+        if(question.equals("What is your name?")) {
+            return "Till S";
+        } else if (question.equals("Who is winning?")) {
+            return "Always Till";
+        } else {
+            return "This question is tough. Please ask ChatGPT.";
+        }
     }
 }
